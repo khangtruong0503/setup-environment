e173a727a75c6797275a92366faaedb59f700c3a
Unknown <khangzhang0503@gmail.com>
2017-10-14 20:58:53 +0700

    ubuntu environment

---
+++ ubuntu_nodejs.sh
@@ -0,0 +1,27 @@
+
+#!/bin/bash
+
+# NOTE: The commands here only applicable for Ubuntu 16.04 Xenial, do not use it for other distros
+
+# Update server to latest packages
+sudo apt update && sudo apt upgrade -y
+
+# Install nginx and git
+sudo apt install -y nginx git
+
+# Install latest NodeJS LTS
+curl -sL https://deb.nodesource.com/setup_6.x | sudo -E bash -
+sudo apt install -y nodejs
+
+# Install MongoDB
+sudo apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 --recv 0C49F3730359A14518585931BC711F9BA15703C6
+
+echo "deb [ arch=amd64,arm64 ] http://repo.mongodb.org/apt/ubuntu xenial/mongodb-org/3.4 multiverse" | sudo tee /etc/apt/sources.list.d/mongodb-org-3.4.list
+sudo apt update && sudo apt install -y mongodb-org
+
+# Start mongod service
+sudo systemctl start mongod
+# Enable mongod service auto restart
+sudo systemctl enable mongod
+
+echo "Ubuntu system initialization complete!"
