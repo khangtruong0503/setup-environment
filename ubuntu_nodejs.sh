a4a169ad8197096c360cc0821a6fbc6537e9bccf
root <root@localhost.localdomain>
2018-05-12 22:29:05 +0700

    do not install mongodb

--- ubuntu_nodejs.sh
+++ ubuntu_nodejs.sh
@@ -54,10 +54,10 @@ curl -sL https://deb.nodesource.com/setup_8.x | sudo -E bash -
 sudo apt install -y nodejs
 
 # Install MongoDB
-sudo apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 --recv 0C49F3730359A14518585931BC711F9BA15703C6
+# sudo apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 --recv 0C49F3730359A14518585931BC711F9BA15703C6
 
-echo "deb [ arch=amd64,arm64 ] http://repo.mongodb.org/apt/ubuntu xenial/mongodb-org/3.4 multiverse" | sudo tee /etc/apt/sources.list.d/mongodb-org-3.4.list
-sudo apt update && sudo apt install -y mongodb-org
+# echo "deb [ arch=amd64,arm64 ] http://repo.mongodb.org/apt/ubuntu xenial/mongodb-org/3.4 multiverse" | sudo tee /etc/apt/sources.list.d/mongodb-org-3.4.list
+# sudo apt update && sudo apt install -y mongodb-org
 
 # Start mongod service
 # sudo systemctl start mongod
